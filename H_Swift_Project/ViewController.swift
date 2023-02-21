3d5a39c0eb1bab60adf6657b23a20f056b52c561
SegmentControl <lwh633@126.com>
2023-02-21 18:58:04 +0800

    first commit

---
+++ H_Swift_Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  H_Swift_Project
+//
+//  Created by RHFlower on 2023/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ H_Swift_ProjectUITests/H_Swift_ProjectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  H_Swift_ProjectUITestsLaunchTests.swift
+//  H_Swift_ProjectUITests
+//
+//  Created by RHFlower on 2023/2/21.
+//
+
+import XCTest
+
+final class H_Swift_ProjectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
